6838ddf782f549118b5539a4ce5763e233263185
pratikrao <pratikrao11082002@gmail.com>
2026-07-03 18:10:29 +0530

    registered models in admin.py

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Transaction, AIInsight
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(AIInsight)
